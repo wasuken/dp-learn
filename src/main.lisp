7f5ca49444e979cb9e08c5c987725d04b581157d
wasuken <wasuken1@gmail.com>
2021-07-03 19:09:34 +0900

    knapsack問題追加。

--- src/main.lisp
+++ src/main.lisp
@@ -1,6 +1,6 @@
 (defpackage dp-learn
   (:use :cl)
-  (:export :frog))
+  (:export :frog :knap))
 (in-package :dp-learn)
 
 ;;; https://atcoder.jp/contests/dp/tasks/dp_a
@@ -10,7 +10,8 @@
 (defun chmin (lst i v)
   (let ((newl lst))
 	(cond ((> (nth i lst) v)
-		   (setf (nth i lst) v))
+		   (setf (nth i lst) v)
+		   lst)
 		  (t lst))))
 
 (defun dp (i lst dpl)
@@ -31,3 +32,48 @@
 		 (dpl (cons 0 (loop for i from 0 to (1- len)
 							   collect most-positive-fixnum))))
 	(car (reverse (dp 1 points dpl)))))
+
+
+;;; 参考: https://www.amazon.co.jp/dp/4065128447
+;;; code 5.7
+
+;;; 問題: https://atcoder.jp/contests/dp/tasks/dp_d
+(defun chmax (dpm i w v)
+  (let ((newdpm dpm))
+	(cond ((< (nth w (nth i dpm)) v)
+		   (setf (nth w (nth i newdpm)) v)
+		   newdpm)
+		  (t dpm)))
+  )
+
+(defun knap-w-dp (i w n wei vw-lst dpm)
+  (cond ((> w wei)
+		 dpm)
+		(t
+		 (let ((newdpm dpm))
+		   (when (>= (- w (car (nth i vw-lst))) 0)
+			 (setf newdpm
+				   (chmax dpm (1+ i) w (+ (nth (- w (car (nth i vw-lst)))
+											   (nth i dpm))
+										  (cdr (nth i vw-lst)))))
+			 )
+		   (knap-w-dp i (1+ w) n wei vw-lst
+					  (chmax newdpm (1+ i) w (nth w (nth i newdpm)))
+					  ))
+		 ))
+  )
+
+(defun knap-n-dp (i n wei vw-lst dpm)
+  (cond ((>= i n)
+		 dpm)
+		(t
+		 (knap-n-dp (1+ i) n wei vw-lst
+					(knap-w-dp i 0 n wei vw-lst dpm)))))
+
+;;; Knapsack
+(defun knap (n wei vw-lst)
+  (let ((dpm (loop for i from 0 to n
+				   collect (loop for j from 0 to wei
+								 collect 0))))
+	(car (reverse (car (reverse (knap-n-dp 0 n wei vw-lst dpm)))))
+	))
--- tests/main.lisp
+++ tests/main.lisp
@@ -4,13 +4,7 @@
         :rove))
 (in-package :dp-learn/tests/main)
 
-;; NOTE: To run this test file, execute `(asdf:test-system :dp-learn)' in your Lisp.
-
-;; (deftest test-target-1
-;;   (testing "should (= 1 1) to be true"
-;;     (ok (= 1 1))))
-
-
+;;; 問題: https://atcoder.jp/contests/dp/tasks/dp_a
 (deftest frog-test
 	(let ((len 4)
 		  (points '(10 30 40 20)))
@@ -19,11 +13,49 @@
 				   30)))
   (let ((len 2)
 		(points '(10 10)))
-	(testing "act1. "
+	(testing "act2. "
 			 (ok (= (frog points))
 				 0)))
   (let ((len 6)
 		(points '(30 10 60 10 60 50)))
-	(testing "act1. "
+	(testing "act3. "
 			 (ok (= (frog points))
 				 40))))
+
+(deftest knap-test
+	(let ((vw-lst '(
+					(3 . 30)
+					(4 . 50)
+					(5 . 60)
+					))
+		  (n 3)
+		  (w 8))
+	  (testing "act1."
+			   (ok (= (knap n w vw-lst)
+					  90))))
+  (let ((vw-lst '(
+				  (1 . 1000000000)
+				  (1 . 1000000000)
+				  (1 . 1000000000)
+				  (1 . 1000000000)
+				  (1 . 1000000000)
+				  ))
+		(n 5)
+		(w 5))
+	(testing "act2."
+			 (ok (= (knap n w vw-lst)
+					5000000000))))
+  (let ((vw-lst '(
+				  (6 . 5)
+				  (5 . 6)
+				  (6 . 4)
+				  (6 . 6)
+				  (3 . 5)
+				  (7 . 2)
+				  ))
+		(n 6)
+		(w 15))
+	(testing "act3."
+			 (ok (= (knap n w vw-lst)
+					17))))
+  )
